89d523d51cec1c92082a0808ea4865fd88f398da
isaac1104 <isaac-kwon@hotmail.com>
2018-08-04 16:35:06 -0700

    "/" route created as home route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
+    const Home = () => {
+      return (
+        <div>
+          <h1>Home Route</h1>
+        </div>
+      );
+    };
+
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
