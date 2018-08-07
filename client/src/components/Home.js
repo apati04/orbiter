9ba7445748774945f36c85b50c2efb6f1d6fcb1c
isaac1104 <isaac-kwon@hotmail.com>
2018-08-07 16:36:51 -0700

    added buttons to routes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Home Route</h1>
+      <Link to='/login'><button>To Login</button></Link>
+      <Link to='/signup'><button>To Signup</button></Link>
     </div>
   );
 };
