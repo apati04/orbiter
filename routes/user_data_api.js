d2e5e54e647d588d25ff4f88f9e21019b62935e9
isaac1104 <isaac-kwon@hotmail.com>
2018-08-07 16:58:16 -0700

    fetching dummy data from firebase

--- routes/user_data_api.js
+++ routes/user_data_api.js
@@ -1,10 +1,6 @@
 const axios = require('axios');
 
 module.exports = app => {
-  app.get('/test', (req, res) => {
-    res.send('test');
-  });
-  
   app.get('/api/users', async (req, res) => {
     const request = await axios.get('https://orbiter-db.firebaseio.com/users.json');
     const { data } = request;
