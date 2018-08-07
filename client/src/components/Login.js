7d7e73c7c69513388f09f32f37d3f239a44dc5fc
isaac1104 <isaac-kwon@hotmail.com>
2018-08-07 16:39:57 -0700

    back to home button added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginForm from './Form/LoginForm';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div>
       <h1>Login Route</h1>
       <LoginForm />
+      <Link to='/'><button>Back to home</button></Link>
     </div>
   );
 };
