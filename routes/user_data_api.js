9532f2dc90b6b37cd512e4a299c6ea4f809cd74b
isaac1104 <isaac-kwon@hotmail.com>
2018-08-07 19:30:09 -0700

    firebase url hidden in dev.js

--- routes/user_data_api.js
+++ routes/user_data_api.js
@@ -1,15 +1,16 @@
 const axios = require('axios');
+const keys = require('../config/keys');
 
 module.exports = app => {
   app.get('/api/users', async (req, res) => {
     //change the firebase url to process.env
-    const request = await axios.get('https://orbiter-db.firebaseio.com/users.json');
+    const request = await axios.get(keys.firebaseURL);
     const { data } = request;
     res.send(data);
   });
 
   app.post('/api/users', async (req, res) => {
-    const request = await axios.post('https://orbiter-db.firebaseio.com/users.json', req.body);
+    const request = await axios.post(keys.firebaseURL, req.body);
     const { data } = request;
     res.send(data);
   });
