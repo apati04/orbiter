b49193029a5a4395d222f86a9ea4545331d20ef0
isaac1104 <isaac-kwon@hotmail.com>
2018-08-07 17:50:11 -0700

    posting to firebase wip

--- routes/user_data_api.js
+++ routes/user_data_api.js
@@ -2,15 +2,15 @@ const axios = require('axios');
 
 module.exports = app => {
   app.get('/api/users', async (req, res) => {
+    //change the firebase url to process.env
     const request = await axios.get('https://orbiter-db.firebaseio.com/users.json');
     const { data } = request;
     res.send(data);
   });
 
-  app.get('/api/add/users', async (req, res) => {
-    console.log(req.query.values);
+  app.post('/api/add/users', async (req, res) => {
+    console.log(req);
     // const request = await axios.post('https://orbiter-db.firebaseio.com/users.json', req.query.values);
-    // console.log(req.query);
     // const { data } = request;
     // res.send(data);
   });
