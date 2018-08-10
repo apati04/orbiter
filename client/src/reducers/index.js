5c146af2858a1d954e4be24fda2dd125df0f5b5d
isaac1104 <isaac-kwon@hotmail.com>
2018-08-10 16:02:19 -0700

    reducer name changed to all users reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import usersDataReducer from './users_data_reducer';
+import allUsersDataReducer from './all_users_data_reducer';
 
 const rootReducer = combineReducers({
   form: formReducer,
-  users: usersDataReducer
+  users: allUsersDataReducer
 });
 
 export default rootReducer;
