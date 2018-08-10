48b61dcb11bc07af0fa061279d6f11f02e74b40b
isaac1104 <isaac-kwon@hotmail.com>
2018-08-09 17:01:38 -0700

    async validation added to username form

---
+++ client/src/components/Form/asyncValidate.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import axios from 'axios';
+
+const asyncValidate = async values => {
+  const request = await axios.get('/api/users');
+  const { data } = request;
+  if (_.map(data).find(user => user.username === values.username)) {
+    throw { username: 'Username is already taken'};
+  }
+};
+
+export default asyncValidate;
