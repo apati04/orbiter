230264a85a46da4d53e372c894b8f1f28457b6ba
Andrew Patipaksiri <apax714@gmail.com>
2018-08-12 14:04:13 -0700

    add auth action types

--- client/src/components/Form/asyncValidate.js
+++ client/src/components/Form/asyncValidate.js
@@ -1,13 +1,12 @@
 import _ from 'lodash';
 import axios from 'axios';
-import debounce from 'debounce-promise';
 
 const asyncValidate = async values => {
   const request = await axios.get('/api/users');
   const { data } = request;
   if (_.map(data).find(user => user.username === values.username)) {
-    throw { username: 'Username is already taken'};
+    throw { username: 'Username is already taken' };
   }
 };
 
-export default debounce(asyncValidate, 500);
+export default asyncValidate;
